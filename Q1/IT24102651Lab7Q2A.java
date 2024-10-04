e6ff8ae362eb93e30caa32e5243c240de7e238ec
it24102651 <it24102651@my.sliit.lk>
2024-10-04 19:40:46 +0530

    Add files via upload

---
+++ Q1/IT24102651Lab7Q2A.java
@@ -0,0 +1,15 @@
+public class IT24102651Lab7Q2A {
+
+    public static void main(String[] args) {
+		
+		for(int rowCount = 1; rowCount <= 4; rowCount++)
+        {
+			for(int columnCount = 1; columnCount <= 5; columnCount++)
+			{
+				System.out.print("$");
+			}
+			
+			System.out.println();
+		}
+    }
+}
